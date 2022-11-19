dcbed7eb414f115f0bea29d6be037374c3279492
cooli7wa <cooli7wa67@163.com>
2022-11-20 06:51:32 +0800

    script

--- _posts/_tools/site_deploy.sh
+++ _posts/_tools/site_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -
 
+sudo jekyll build
+
 sudo rm -rf /var/www/cooli7wa.com
-sudo cp -R /home/lighthouse/liuqi/cooli7wa.github.io/_site /var/www/cooli7wa.com
+sudo cp -R ./_site /var/www/cooli7wa.com
+sudo chmod -R a+r /var/www/cooli7wa.com
 
 sudo service nginx restart
