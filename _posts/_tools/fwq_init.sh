8f176faba680a96d883a611da356f0a263a00e23
cooli7wa <cooli7wa67@163.com>
2022-11-20 10:14:52 +0800

    script

--- _posts/_tools/fwq_init.sh
+++ _posts/_tools/fwq_init.sh
@@ -2,9 +2,10 @@
 
 
 sudo apt-get update
-sudo apt install -y gem ruby nodejs ruby-dev jekyll
+sudo apt install -y gem ruby nodejs ruby-dev 
 
 sudo gem install bundler:2.3.6
+sudo gem install jekyll
 
 sudo bundle install
 sudo bundle update
